7fa61d44876576d6a7bd11f636f6878b0672b60e
Denise Ratasich <12098667+dratasich@users.noreply.github.com>
2025-10-26 10:23:54 +0100

    Remove debug parts

--- src/main.cpp
+++ src/main.cpp
@@ -10,30 +10,13 @@
 /* Number of LEDs in the strip */
 #define LED_COUNT 28
 
+// Short initialization test of the LEDs
 #define LED_TEST_ENABLE true
-#define LED_TEST_DELAY 500
+#define LED_TEST_DELAY 250
 
-//#define DEBUG true
 
 /* LED strip data */
 CRGB leds[LED_COUNT];
-
-/* Test LEDs by cycling through colors and LEDs */
-void testLEDs()
-{
-  const CRGB colors[] = {CRGB::Red, CRGB::Green, CRGB::Blue};
-  for (const auto &color : colors)
-  {
-    for (int i = 0; i < LED_COUNT; i++)
-    {
-      leds[i] = color;
-      FastLED.show();
-      delay(LED_TEST_DELAY);
-      // toggle initialization LED to show progress
-      digitalWrite(13, !digitalRead(13));
-    }
-  }
-}
 
 /* Set all LEDs to a specific color */
 void allLEDsToColor(const CRGB &color)
@@ -45,6 +28,7 @@ void allLEDsToColor(const CRGB &color)
   FastLED.show();
 }
 
+/* Test all LEDs by cycling through Red, Green, and Blue */
 void testAllLEDs() {
   allLEDsToColor(CRGB::Red);
   delay(LED_TEST_DELAY);
@@ -59,37 +43,20 @@ int main()
   // setup Arduino core timers such that delay() works as expected
   init();
 
-  // use D13 to indicate initialization status
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-
-#ifdef DEBUG
-  Serial.begin(9600);
-#endif
-
   FastLED.addLeds<LED_CONTROLLER, LED_PINS, LED_COLOR_ORDER>(leds, LED_COUNT);
   FastLED.setBrightness(64);
 
-#ifdef DEBUG
-  Serial.println("initialized");
-#endif
-
-  // startup tests (run through all colors and leds)
+  // initialization test
   if (LED_TEST_ENABLE)
   {
-    //testLEDs();
     testAllLEDs();
   }
-
-#ifdef DEBUG
-  Serial.println("tested");
-#endif
 
   // set LEDs to a nice color
   allLEDsToColor(CRGB::Orange4);
 
-  digitalWrite(13, LOW); // turn off status LED
-
-  while (1)
-    ;
+  // idle forever
+  for (;;) {
+    delay(1000);
+  }
 }
